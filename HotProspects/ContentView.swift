82c56004f6bdb95785c399bdf3eb5d12a6564083
Jules Lee <jackconsolidated@gmail.com>
2020-01-31 19:20:02 +0800

    QR Code now works

--- HotProspects/ContentView.swift
+++ HotProspects/ContentView.swift
@@ -7,10 +7,68 @@
 //
 
 import SwiftUI
+import UserNotifications
+import SamplePackage
+
+
+enum NetworkError: Error {
+    case badURL, requestFailed, unknown
+}
 
 struct ContentView: View {
+    var prospects = Prospects()
+    
     var body: some View {
-        Text("Hello, World!")
+        TabView {
+            ProspectsView(filter: .none)
+                .tabItem {
+                    Image(systemName: "person.3")
+                    Text("Everyone")
+                }
+            ProspectsView(filter: .none)
+                .tabItem {
+                    Image(systemName: "checkmark.circle")
+                    Text("Contacted")
+                }
+            ProspectsView(filter: .none)
+                .tabItem {
+                    Image(systemName: "questionmark.diamond")
+                    Text("Uncontacted")
+                }
+            MeView()
+                .tabItem {
+                    Image(systemName: "person.crop.square")
+                    Text("Me")
+                }
+        }
+        .environmentObject(prospects)
+
+        
+    }
+    
+    func fetchData(from urlString: String, completion: @escaping (Result<String, NetworkError>) -> Void) {
+        // check the URL is OK, otherwise return with a failure
+        guard let url = URL(string: urlString) else {
+            completion(.failure(.badURL))
+            return
+        }
+
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            // the task has completed – push our work back to the main thread
+            DispatchQueue.main.async {
+                if let data = data {
+                    // success: convert the data to a string and send it back
+                    let stringData = String(decoding: data, as: UTF8.self)
+                    completion(.success(stringData))
+                } else if error != nil {
+                    // any sort of network failure
+                    completion(.failure(.requestFailed))
+                } else {
+                    // this ought not to be possible, yet here we are
+                    completion(.failure(.unknown))
+                }
+            }
+        }.resume()
     }
 }
 
